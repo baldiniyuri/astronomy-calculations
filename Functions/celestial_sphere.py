034f318f16a166cc038acfd9ad594bf4bb64dac6
Yuri <yuriabaldini@gmail.com>
2023-03-02 20:43:18 -0300

    created celestial class and equatorial class

---
+++ Functions/celestial_sphere.py
@@ -0,0 +1,23 @@
+
+
+class CelestialSphere():
+    nadir = -90
+    nadir_north_distance = 0
+    nadir_south_distance = -180
+    north_azimuth = 360
+    south_azimuth = 180
+    east_azimuth = 90
+    west_azimuth = 270
+    top_distance_zenith = 90
+    zenith = 90
+    zenith_north_distance = 0
+    zenith_south_distance = 180
+    
+    
+    def __init__(self, ):
+        ...
+
+
+
+
+        
---
+++ Functions/equatorial_coordinates.py
@@ -0,0 +1,9 @@
+
+
+class EquatorialCoordinates:
+    declination_north = 90
+    declination_south = -90
+
+
+    def __init__(self, gamma):
+        self.straight_ascension = gamma 
