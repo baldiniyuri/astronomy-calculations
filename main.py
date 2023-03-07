0bab3d5a9eab5ace4db4acbea233bc6fce782f4d
Yuri <yuriabaldini@gmail.com>
2023-03-07 20:07:51 -0300

    created image analysis class

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from Functions.spherical_trigonometry import start_trigonometry
 from Functions.astronomy_calculations import start_astronomy_calculations
-
+from Functions.images_analysis import start_image_analysis
 
 def main():
     print("Running...")
     print("Select program.")
-    print("1 For Spherical Trigonometry")
-    print("2 For Astronomical Calculations")
+    print("1 For Spherical Trigonometry.")
+    print("2 For Astronomical Calculations.")
+    print("3 For Image Analysis.")
     program = int(input())
+    
     if program == 1:
         start_trigonometry()
     elif program == 2:
         start_astronomy_calculations()
+    elif program == 3:
+        start_image_analysis()
     else:
         print("Invalid program input.")
         print("Terminating program.")
