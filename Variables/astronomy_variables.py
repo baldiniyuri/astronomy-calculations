70ef614f2b2ed4b4a40f9789295c7e6c7bcb058e
Yuri <yuriabaldini@gmail.com>
2023-03-09 21:44:05 -0300

    created star specter

--- Variables/astronomy_variables.py
+++ Variables/astronomy_variables.py
@@ -1,4 +1,4 @@
-import astronomy
+
 
 
 LIGHT_YEAR_METRES = 9460730472580800
@@ -6,3 +6,5 @@ LIGHT_YEAR_KM = 9460730472580
 ASTRONOMICAL_UNIT = 63241.077088 # Equals 1 Light Year
 ASTRONOMICAL_UNIT_KM = 150000000 
 PARSEC = 0.0000048481 # Equals 1 Astronomical unit
+
+ELEMENTS = ["Hydrogen", "Helium", "Carbon", "Iron", "Calcium", "Sodium", "Magnesium", "Oxygen"]
