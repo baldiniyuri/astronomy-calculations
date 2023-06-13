3932adfd39c7e19332881e00892f0135495576c4
Yuri <yuriabaldini@gmail.com>
2023-06-13 11:56:10 -0300

    created annotation on star distance

--- Functions/star_distance.py
+++ Functions/star_distance.py
@@ -2,21 +2,21 @@ import math
 
 
 class StarDistance:
-    def __init__(self, luminosity: float, brightness: float):
+    def __init__(self, luminosity: float, brightness: float) -> None:
         self.luminosity = luminosity #watts value
         self.brightness = brightness #watts value
         self.sun_watts = 3.828e26 # Value of sun luminosity in watts.
 
 
-    def convert_luminosity_from_watts_to_solar_luminosity(self):
+    def convert_luminosity_from_watts_to_solar_luminosity(self) -> float:
         return self.luminosity / self.sun_watts
     
 
-    def inverse_square(self, L_solar):
+    def inverse_square(self, L_solar) -> float:
         return math.sqrt(L_solar / (4 * math.pi * self.brightness))
     
     
-    def calculate_star_distance(self):
+    def calculate_star_distance(self) -> float:
         L_solar = self.convert_luminosity_from_watts_to_solar_luminosity()
         
         distance_parsecs = self.inverse_square(L_solar)
